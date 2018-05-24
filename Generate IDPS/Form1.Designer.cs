f6cf2f14399db4519a714d5ce88044f01bd4c221
xXxTheDarkprogramerxXx <eonvwyk@gmail.com>
2018-05-24 21:02:51 +0200

    Initial commit

---
+++ Generate IDPS/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace Generate_IDPS
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(33, 26);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(53, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Region";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "JP",
+            "US/CA",
+            "AU/NZ",
+            "UK/IE",
+            "EU/ME/AF",
+            "KR",
+            "SG/MY",
+            "TW",
+            "RU/UA/IN/C(Asia)",
+            "CN",
+            "MX/SA",
+            "HK"});
+            this.comboBox1.Location = new System.Drawing.Point(149, 22);
+            this.comboBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(160, 24);
+            this.comboBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(268, 265);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(42, 17);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "#FST";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(75, 258);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(159, 28);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gen IDPS";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.backgroundWorker1.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(124, 63);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 17);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Serial";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(37, 82);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(272, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(41, 135);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(77, 17);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Model type";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Items.AddRange(new object[] {
+            "01",
+            "02",
+            "03",
+            "04",
+            "05",
+            "06",
+            "07",
+            "08",
+            "09",
+            "0A",
+            "0B",
+            "0C",
+            "0D",
+            "0E",
+            "0F",
+            "13"});
+            this.comboBox2.Location = new System.Drawing.Point(128, 132);
+            this.comboBox2.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(160, 24);
+            this.comboBox2.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(41, 171);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 17);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "CECH";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.comboBox3.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Items.AddRange(new object[] {
+            "All",
+            "CECH-20xxA",
+            "CECH-20xxB",
+            "CECH-21xxA",
+            "CECH-21xxB",
+            "CECH-25xxA",
+            "CECH-25xxB",
+            "CECH-30xxA",
+            "CECH-30xxB",
+            "CECH-40xxA",
+            "CECH-40xxB",
+            "CECH-40xxC",
+            "CECH-42xxA",
+            "CECH-42xxB",
+            "CECH-42xxC",
+            "CECH-43xxA",
+            "CECH-43xxB",
+            "CECH-43xxC"});
+            this.comboBox3.Location = new System.Drawing.Point(128, 167);
+            this.comboBox3.Margin = new System.Windows.Forms.Padding(4);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(160, 24);
+            this.comboBox3.TabIndex = 108;
+            this.comboBox3.Tag = "";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(329, 302);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "IDPS Gen_BETA";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox3;
+
+    }
+}
+
---
+++ Generate IDPS/Form1.cs
@@ -0,0 +1,192 @@
+﻿
+//▒██   ██▒▒██   ██▒▒██   ██▒   ▄▄▄█████▓ ██░ ██ ▓█████    ▓█████▄  ▄▄▄       ██▀███   ██ ▄█▀ ██▓███   ██▀███   ▒█████    ▄████  ██▀███   ▄▄▄       ███▄ ▄███▓▓█████  ██▀███     ▒██   ██▒▒██   ██▒▒██   ██▒
+//▒▒ █ █ ▒░▒▒ █ █ ▒░▒▒ █ █ ▒░   ▓  ██▒ ▓▒▓██░ ██▒▓█   ▀    ▒██▀ ██▌▒████▄    ▓██ ▒ ██▒ ██▄█▒ ▓██░  ██▒▓██ ▒ ██▒▒██▒  ██▒ ██▒ ▀█▒▓██ ▒ ██▒▒████▄    ▓██▒▀█▀ ██▒▓█   ▀ ▓██ ▒ ██▒   ▒▒ █ █ ▒░▒▒ █ █ ▒░▒▒ █ █ ▒░
+//░░  █   ░░░  █   ░░░  █   ░   ▒ ▓██░ ▒░▒██▀▀██░▒███      ░██   █▌▒██  ▀█▄  ▓██ ░▄█ ▒▓███▄░ ▓██░ ██▓▒▓██ ░▄█ ▒▒██░  ██▒▒██░▄▄▄░▓██ ░▄█ ▒▒██  ▀█▄  ▓██    ▓██░▒███   ▓██ ░▄█ ▒   ░░  █   ░░░  █   ░░░  █   ░
+// ░ █ █ ▒  ░ █ █ ▒  ░ █ █ ▒    ░ ▓██▓ ░ ░▓█ ░██ ▒▓█  ▄    ░▓█▄   ▌░██▄▄▄▄██ ▒██▀▀█▄  ▓██ █▄ ▒██▄█▓▒ ▒▒██▀▀█▄  ▒██   ██░░▓█  ██▓▒██▀▀█▄  ░██▄▄▄▄██ ▒██    ▒██ ▒▓█  ▄ ▒██▀▀█▄      ░ █ █ ▒  ░ █ █ ▒  ░ █ █ ▒ 
+//▒██▒ ▒██▒▒██▒ ▒██▒▒██▒ ▒██▒     ▒██▒ ░ ░▓█▒░██▓░▒████▒   ░▒████▓  ▓█   ▓██▒░██▓ ▒██▒▒██▒ █▄▒██▒ ░  ░░██▓ ▒██▒░ ████▓▒░░▒▓███▀▒░██▓ ▒██▒ ▓█   ▓██▒▒██▒   ░██▒░▒████▒░██▓ ▒██▒   ▒██▒ ▒██▒▒██▒ ▒██▒▒██▒ ▒██▒
+//▒▒ ░ ░▓ ░▒▒ ░ ░▓ ░▒▒ ░ ░▓ ░     ▒ ░░    ▒ ░░▒░▒░░ ▒░ ░    ▒▒▓  ▒  ▒▒   ▓▒█░░ ▒▓ ░▒▓░▒ ▒▒ ▓▒▒▓▒░ ░  ░░ ▒▓ ░▒▓░░ ▒░▒░▒░  ░▒   ▒ ░ ▒▓ ░▒▓░ ▒▒   ▓▒█░░ ▒░   ░  ░░░ ▒░ ░░ ▒▓ ░▒▓░   ▒▒ ░ ░▓ ░▒▒ ░ ░▓ ░▒▒ ░ ░▓ ░
+//░░   ░▒ ░░░   ░▒ ░░░   ░▒ ░       ░     ▒ ░▒░ ░ ░ ░  ░    ░ ▒  ▒   ▒   ▒▒ ░  ░▒ ░ ▒░░ ░▒ ▒░░▒ ░       ░▒ ░ ▒░  ░ ▒ ▒░   ░   ░   ░▒ ░ ▒░  ▒   ▒▒ ░░  ░      ░ ░ ░  ░  ░▒ ░ ▒░   ░░   ░▒ ░░░   ░▒ ░░░   ░▒ ░
+// ░    ░   ░    ░   ░    ░       ░       ░  ░░ ░   ░       ░ ░  ░   ░   ▒     ░░   ░ ░ ░░ ░ ░░         ░░   ░ ░ ░ ░ ▒  ░ ░   ░   ░░   ░   ░   ▒   ░      ░      ░     ░░   ░     ░    ░   ░    ░   ░    ░  
+// ░    ░   ░    ░   ░    ░               ░  ░  ░   ░  ░      ░          ░  ░   ░     ░  ░               ░         ░ ░        ░    ░           ░  ░       ░      ░  ░   ░         ░    ░   ░    ░   ░    ░  
+//                                                          ░                                                        
+
+//====================================================================================================================================================================================================================
+//                                          Under Proetection From Myself 
+//====================================================================================================================================================================================================================
+
+///Dam My Name Looks Sexy hahah 
+///Anyway Marco here is the source i have so far it still needs a few fixes as it needs the model seletion never goes beyond 
+///offset 0x30 Correlating with Model Number as You Suggested 
+/// No We Need To Check Via Serial For Any Indications
+/// e.g if your serial is ############ the part before the cech i belive might be the following parts of the idps but ill look into this more after work 
+
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Generate_IDPS
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        int count=0;
+        string finalString  = ""; //Basic Outlay of the System IDPS's ? Seems Each Region Starts with 8 then has its own value 
+        public byte[] ReadHexStringToByte(string Input)
+        {
+            Input = Input.ToUpper().Trim();
+            string ValidChar = "0123456789ABCDEF";
+            foreach (char character in Input)
+            {
+                if (!ValidChar.Contains(character))
+                    throw new Exception("Invalid HexString");
+            }
+            if (Input.Length == 0)
+                throw new Exception("Hexstring resulted in NULL");
+            if (!(Input.Length % 2 == 0))
+                Input = "0" + Input;
+            byte[] ResultBytes = new byte[(Input.Length / 2)];
+            int index = 0;
+            for (int i = 0; i <= Input.Length - 1; i +=2)
+            {
+                try
+                {
+                    ResultBytes[index] = Convert.ToByte(Input.Substring(i, 2), 16);
+                    index += 1;
+                }
+                catch
+                {
+                    return ResultBytes;
+                }
+            }
+            return ResultBytes;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            
+            //Run In A Bacgoruding Worker To Avoid Prying Eyes 
+            backgroundWorker1.RunWorkerAsync();
+            
+         }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Allways Set The COmbobox And Dont Allow User Text 
+            comboBox1.SelectedIndex = 0;
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+
+            {
+                //Magic Static 
+
+                finalString = "00000001008";
+
+                int switchvar = 0;
+                comboBox1.Invoke(new Action(() => switchvar = comboBox1.SelectedIndex));
+                //Region Codes By MARCO !!!! Your A Legend Man
+                switch (switchvar)
+                {
+                    case 0:
+                        finalString += "3"; //JP
+                        break;
+                    case 1:
+                        finalString += "4";//US/CA
+                        break;
+                    case 2:
+                        finalString += "9";//AU/NZ
+                        break;
+                    case 3:
+                        finalString += "7";//UK/IE
+                        break;
+                    case 4:
+                        finalString += "5";//EU/ME/AF
+                        break;
+                    case 5:
+                        finalString += "6";//KR
+                        break;
+                    case 6:
+                        finalString += "A";//SG/MY
+                        break;
+                    case 7:
+                        finalString += "B";//TW
+                        break;
+                    case 8:
+                        finalString += "C";//RU/UA/IN/C(Asia)
+                        break;
+                    case 9:
+                        finalString += "D";//CN
+                        break;
+                    case 10:
+                        finalString += "8";//MX/SA   I Missed One -_- HK not important as no one from testers is HK
+                        break;
+                    default:
+                        MessageBox.Show("Something Went Wrong or your just stupid as hell \n\n sence of humor detected ?");
+                        break;
+
+                }
+                //Seperator 
+                finalString += "00";
+
+                //Model type 
+                //this should actually be gotten from model number but ehhh whatever
+
+                comboBox2.Invoke(new Action(() => finalString += comboBox2.Text));
+
+                //chassis check 
+
+                var oldArray = new int?[] { 1, 2, 3, 4, 5, 6 };
+
+                /*
+                 * 
+                 * (0x1400 >> 0xA) = 5d or 0x1400h thus 1010000000000b then shift the pointer left by 0xDh resulting in 101b or 0x5h
+                 * 
+                 */
+
+
+
+                var chars = "0123456789ABCDEF";
+
+                var stringChars = new char[12];
+
+                var random = new Random();
+
+
+                /// This Needs To CHange As This Links To The Model Number Meaning That After FinalString 00000108[region] comes the model 
+                /// never goes higher that 0x30 thanks pink that was the final conclusion i needed 
+                /// MARCO AND PINK !! ROCK ON
+
+                for (int i = 0; i < stringChars.Length; i++)
+                {
+                    stringChars[i] = chars[random.Next(chars.Length)];
+                }
+                count++;
+                finalString += new String(stringChars).ToString();
+            }
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            MessageBox.Show("Found Idps.bin after " + count + " tries ");
+            string Lines = finalString;
+
+
+            var result = ReadHexStringToByte(finalString);
+
+            System.IO.File.WriteAllBytes(Application.StartupPath + @"\\idps.bin", result);
+
+            MessageBox.Show(BitConverter.ToString(System.IO.File.ReadAllBytes(Application.StartupPath + @"\\idps.bin")));
+        }
+    }
+}
